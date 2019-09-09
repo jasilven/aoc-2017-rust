ef2f95ac94dd5788fb324ea8e7cb6617f225aa24
jasilven <jasilven@gmail.com>
2019-09-09 16:30:03 +0300

    day1 part2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -21,9 +21,22 @@ fn solve1(digits: &[u8]) -> usize {
     result
 }
 
+fn solve2(digits: &[u8]) -> usize {
+    let mut result = 0usize;
+    let cnt = digits.len();
+    let offset = cnt / 2;
+    for (index, digit) in digits.iter().enumerate() {
+        if digits[(index + offset) % cnt] == *digit {
+            result += *digit as usize;
+        }
+    }
+    result
+}
+
 fn main() {
     let digits = parse_input("resources/day1_input.txt");
     println!("Part 1: {}", solve1(&digits));
+    println!("Part 2: {}", solve2(&digits));
 }
 
 #[cfg(test)]
@@ -38,5 +51,19 @@ mod tests {
         assert_eq!(3, solve1(&digits1));
         assert_eq!(4, solve1(&digits2));
         assert_eq!(0, solve1(&digits3));
+    }
+
+    #[test]
+    fn test_solve2() {
+        let digits1 = vec![1, 2, 1, 2];
+        let digits2 = vec![1, 2, 2, 1];
+        let digits3 = vec![1, 2, 3, 4, 2, 5];
+        let digits4 = vec![1, 2, 3, 1, 2, 3];
+        let digits5 = vec![1, 2, 1, 3, 1, 4, 1, 5];
+        assert_eq!(6, solve2(&digits1));
+        assert_eq!(0, solve2(&digits2));
+        assert_eq!(4, solve2(&digits3));
+        assert_eq!(12, solve2(&digits4));
+        assert_eq!(4, solve2(&digits5));
     }
 }
