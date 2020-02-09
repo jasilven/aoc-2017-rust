268312f4f58c8c1c32bf21d85367dc9de01428da
jasilven <jasilven@gmail.com>
2020-02-09 19:50:32 +0200

    day17, part2

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,4 +1,7 @@
-fn solve1(steps: usize, last_val: usize) -> Result<usize, Box<dyn std::error::Error>> {
+fn solve1(
+    steps: usize,
+    last_val: usize,
+) -> Result<(Vec<usize>, usize), Box<dyn std::error::Error>> {
     let mut buf: Vec<usize> = vec![0];
     let mut curpos = 0;
 
@@ -7,11 +10,28 @@ fn solve1(steps: usize, last_val: usize) -> Result<usize, Box<dyn std::error::Er
         buf.insert(curpos, i);
     }
 
-    Ok(buf[(curpos + 1) % buf.len()])
+    Ok((buf, curpos))
+}
+
+fn solve2(steps: usize, last_val: usize) -> usize {
+    let mut curpos = 0;
+    let mut result = 0;
+
+    for i in 1..=last_val {
+        curpos = (curpos + steps + 1) % i;
+        if curpos == 0 {
+            result = i;
+        }
+    }
+
+    result
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("part 1: {}", solve1(355, 2017)?);
+    let (buf, curpos) = solve1(355, 2017)?;
+
+    println!("part 1: {}", buf[(curpos + 1) % buf.len()]);
+    println!("part 2: {}", solve2(355, 50_000_000));
 
     Ok(())
 }
