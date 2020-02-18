c583bccbc14e0488816da652c9aa3edcf804c733
jasilven <jasilven@gmail.com>
2020-02-18 16:37:05 +0200

    day 20, part1

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -14,14 +14,14 @@ fn parse_input(fname: &str) -> Result<Vec<Particle>, String> {
     let mut result = Vec::new();
 
     for line in reader.lines() {
-        let line = line.map_err(|_| "Line parse error".to_string())?;
-        let nums = line
+        let nums: Result<Vec<isize>, std::num::ParseIntError> = line
+            .map_err(|_| "Line parse error".to_string())?
             .chars()
             .filter(|ch| ch.is_digit(10) || ch == &'-' || ch == &',')
-            .collect::<String>();
-
-        let nums: Result<Vec<isize>, std::num::ParseIntError> =
-            nums.split(',').map(|num| num.parse::<isize>()).collect();
+            .collect::<String>()
+            .split(',')
+            .map(|num| num.parse::<isize>())
+            .collect();
 
         match nums {
             Ok(vec) => {
@@ -38,9 +38,26 @@ fn parse_input(fname: &str) -> Result<Vec<Particle>, String> {
     Ok(result)
 }
 
-fn main() -> Result<(), String> {
-    let input = parse_input("resources/day20_testdata.txt")?;
+fn solve1(particles: &[Particle]) -> usize {
+    let mut particle = 0;
+    let mut min_acceleration = usize::max_value();
 
-    println!("{:?}", &input);
+    for (i, p) in particles.iter().enumerate() {
+        let a = (p.acceleration.0.abs() + p.acceleration.1.abs() + p.acceleration.2.abs()).abs()
+            as usize;
+        if a < min_acceleration {
+            min_acceleration = a;
+            particle = i;
+        }
+    }
+
+    particle
+}
+
+fn main() -> Result<(), String> {
+    let input = parse_input("resources/day20_input.txt")?;
+
+    println!("part 1: {:?}", solve1(&input));
+
     Ok(())
 }
