3b215ba7e108f8454caaa456a68f638a2ff6d5a8
jasilven <jasilven@gmail.com>
2020-02-19 21:34:06 +0200

    day20, part2

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,40 +1,39 @@
+use failure::Error;
+use std::collections::HashSet;
 use std::fs;
 use std::io::{BufRead, BufReader};
 
-#[derive(Debug)]
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, PartialEq, Ord, PartialOrd, Eq, Hash, Clone)]
 struct Particle {
-    position: (isize, isize, isize),
-    velocity: (isize, isize, isize),
-    acceleration: (isize, isize, isize),
+    position: (i64, i64, i64),
+    velocity: (i64, i64, i64),
+    acceleration: (i64, i64, i64),
 }
 
-fn parse_input(fname: &str) -> Result<Vec<Particle>, String> {
-    let file = fs::File::open(fname).map_err(|e| e.to_string())?;
+fn parse_input(fname: &str) -> Result<Vec<Particle>> {
+    let file = fs::File::open(fname)?;
     let reader = BufReader::new(file);
     let mut result = Vec::new();
 
     for line in reader.lines() {
-        let nums: Result<Vec<isize>, std::num::ParseIntError> = line
-            .map_err(|_| "Line parse error".to_string())?
+        let nums: std::result::Result<Vec<i64>, std::num::ParseIntError> = line?
             .chars()
             .filter(|ch| ch.is_digit(10) || ch == &'-' || ch == &',')
             .collect::<String>()
             .split(',')
-            .map(|num| num.parse::<isize>())
+            .map(|num| num.parse::<i64>())
             .collect();
 
-        match nums {
-            Ok(vec) => {
-                result.push(Particle {
-                    position: (vec[0], vec[1], vec[2]),
-                    velocity: (vec[3], vec[4], vec[5]),
-                    acceleration: (vec[6], vec[7], vec[8]),
-                });
-            }
-            Err(e) => return Err(e.to_string()),
-        }
-    }
+        let nums = nums?;
 
+        result.push(Particle {
+            position: (nums[0], nums[1], nums[2]),
+            velocity: (nums[3], nums[4], nums[5]),
+            acceleration: (nums[6], nums[7], nums[8]),
+        });
+    }
     Ok(result)
 }
 
@@ -54,10 +53,38 @@ fn solve1(particles: &[Particle]) -> usize {
     particle
 }
 
-fn main() -> Result<(), String> {
-    let input = parse_input("resources/day20_input.txt")?;
+fn solve2(particles: &mut Vec<Particle>) -> usize {
+    for _ in 0..1000 {
+        for p in particles.iter_mut() {
+            p.velocity.0 += p.acceleration.0;
+            p.velocity.1 += p.acceleration.1;
+            p.velocity.2 += p.acceleration.2;
+            p.position.0 += p.velocity.0;
+            p.position.1 += p.velocity.1;
+            p.position.2 += p.velocity.2;
+        }
 
-    println!("part 1: {:?}", solve1(&input));
+        particles.sort_by(|a, b| a.position.cmp(&b.position));
+
+        let mut duplicates = HashSet::new();
+
+        for i in 1..particles.len() {
+            if particles[i - 1].position == particles[i].position {
+                duplicates.insert(particles[i - 1].clone());
+                duplicates.insert(particles[i].clone());
+            }
+        }
+        particles.retain(|p| !duplicates.contains(&p));
+    }
+
+    particles.len()
+}
+
+fn main() -> Result<()> {
+    let mut particles = parse_input("resources/day20_input.txt")?;
+
+    println!("part 1: {:?}", solve1(&particles));
+    println!("part 2: {:?}", solve2(&mut particles));
 
     Ok(())
 }
